71af15ed868b3b0fa1a553bb4cdcb14d9d8e2856
dvyne10 <dagadu.divine@gmail.com>
2023-04-02 15:42:43 -0400

    chore: testing sample codes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("what course are you taking?");
+        String results = input.nextLine();
+        System.out.println("hmmm "+results+", that's a nice course");
     }
 }
